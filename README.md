da6c37356792e1c65283538a934e203a7edc674c
cugwu13 <chideraair@gmail.com>
2022-04-01 09:09:51 -0500

    configured new API and added styling to app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nba-app
+
 Compare stats between two NBA players
+
+TODO:
+
+-   animate background
+-   set sig figs on stats
+-   fix fade-in issues with stat cards
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 class Player {
-    constructor(first, last) {
+    constructor(first, last, season) {
         this.first = first;
         this.last = last;
+        this.season = season;
         this.options = {
             mode: 'cors',
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Host': 'free-nba.p.rapidapi.com',
-                'X-RapidAPI-Key':
-                    '2d72ce2d88mshb9317c7d2ad860fp1af8a0jsn8c4114e65a8d',
-            },
         };
     }
 
-    get playerName() {
-        return `${this.first} ${this.last}`;
+    async fullName() {
+        const response = await fetch(
+            `https://www.balldontlie.io/api/v1/players?search=${this.first}%20${this.last}`,
+            this.options
+        );
+        const data = await response.json();
+        const dataObj = data.data[0];
+        const name = `${dataObj.first_name} ${dataObj.last_name}`;
+
+        return name;
     }
 
     async getPlayerID() {
@@ -26,7 +29,7 @@ class Player {
 
         try {
             const response = await fetch(
-                `https://free-nba.p.rapidapi.com/players?page=0&per_page=25&search=${this.first}%20${this.last}`,
+                `https://www.balldontlie.io/api/v1/players?search=${this.first}%20${this.last}`,
                 this.options
             );
             const data = await response.json();
@@ -37,97 +40,68 @@ class Player {
         }
     }
 
-    async getPlayerStats() {
-        try {
-            const id = await this.getPlayerID();
-            const response = await fetch(
-                `https://free-nba.p.rapidapi.com/stats?page=0&per_page=100&player_ids[]=${id}`,
-                this.options
-            );
-            const data = await response.json();
-
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-}
-
-class PlayerStats extends Player {
-    season;
-
-    constructor(first, last, season) {
-        super(first, last);
-        this.season = season;
+    isPlayer(player) {
+        return (
+            player.firstName.toLowerCase() === this.first.toLowerCase() &&
+            player.lastName.toLowerCase() === this.last.toLowerCase()
+        );
     }
 
-    async getPlayerStats() {
-        try {
-            const id = await this.getPlayerID();
-            const response = await fetch(
-                `https://free-nba.p.rapidapi.com/stats?page=0&per_page=100&seasons[]=${this.season}&player_ids[]=${id}`,
-                this.options
-            );
-            const data = await response.json();
+    async getImgId() {
+        const response = await fetch(
+            `http://data.nba.net/data/10s/prod/v1/${this.season}/players.json`,
+            this.options
+        );
+        const data = await response.json();
+        const players = data.league.standard;
+        const id = players.filter(this.isPlayer.bind(this))[0].personId;
 
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
+        return id;
+    }
+
+    async getPlayerPic() {
+        const id = await this.getImgId();
+        const pic = await fetch(
+            `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${id}.png`,
+            this.options
+        );
+
+        return pic.url;
     }
 
     async getSeasonAvg() {
         try {
-            const games = await this.getPlayerStats();
-            const gamesArr = games.data.filter((game) => game.min !== '');
-            const numOfGames = gamesArr.length;
+            const id = await this.getPlayerID();
+            const response = await fetch(
+                `https://www.balldontlie.io/api/v1/season_averages?season=${this.season}&player_ids[]=${id}`,
+                this.options
+            );
+            const data = await response.json();
+            const dataObj = data.data[0];
+            const majorData = (({
+                pts,
+                reb,
+                ast,
+                fg_pct,
+                fg3_pct,
+                ft_pct,
+            }) => ({ pts, reb, ast, fg_pct, fg3_pct, ft_pct }))(dataObj);
 
-            const avgPts = gamesArr.reduce((a, b) => a + b.pts, 0) / numOfGames;
-
-            console.log(gamesArr);
-            console.log(numOfGames);
-            console.log(avgPts);
+            return majorData;
         } catch (error) {
             console.log(error);
         }
     }
 }
 
-const x = new PlayerStats('trae', 'young', '2021');
-x.getSeasonAvg();
-
 const AppController = (() => {
-    function getPlayerStats(first, last, season) {
-        const player = new PlayerStats(first, last, season);
+    let player1;
+    let player2;
 
-        return player.getPlayerStats();
-    }
+    function createPlayerObj(name, season) {
+        const nameObj = getPlayerName(name);
 
-    function runApp() {
-        DomInterface.userStats();
-    }
-
-    return { getPlayerStats, runApp };
-})();
-
-const DomInterface = (() => {
-    function userStats() {
-        const btnEL = (btn) => {
-            btn.addEventListener('click', (e) => {
-                const parent = btn.parentElement;
-                const nameInput = parent.firstElementChild.lastElementChild;
-                const seasonInput =
-                    parent.firstElementChild.nextElementSibling
-                        .lastElementChild;
-                displayStats(nameInput.value, seasonInput.value);
-                e.preventDefault();
-            });
-        };
-
-        const btns = document.querySelectorAll('button');
-        btns.forEach((btn) => {
-            btnEL(btn);
-        });
+        return new Player(nameObj.first, nameObj.last, season);
     }
 
     function getPlayerName(fullName) {
@@ -136,19 +110,139 @@ const DomInterface = (() => {
         return { first, last };
     }
 
-    async function displayStats(fullName, season) {
-        const obj = getPlayerName(fullName);
-        const div = document.createElement('div');
-        const stats = await AppController.getPlayerStats(
-            obj.first,
-            obj.last,
-            season
-        );
-        div.textContent = JSON.stringify(stats);
-        document.body.appendChild(div);
+    function runApp() {
+        DomInterface.loadDom();
     }
 
-    return { userStats };
+    return { player1, player2, createPlayerObj, runApp };
+})();
+
+const DomInterface = (() => {
+    // Top-level content container
+    const contentContainer = document.querySelector('.content-container');
+
+    // General fade in animation for elements
+    function fadeIn(el) {
+        el.style.opacity = 0;
+        (function fade() {
+            let val = parseFloat(el.style.opacity);
+            if (!((val += 0.1) > 1)) {
+                el.style.opacity = val;
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    // General fade out animation for elements
+    function fadeOut(el) {
+        el.style.opacity = 1;
+        (function fade() {
+            if ((el.style.opacity -= 0.1) < 0) {
+                el.style.display = 'none';
+            } else {
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    const loadDom = () => {
+        const compareStats = document.querySelector('#compare-stats');
+        compareStats.addEventListener('click', async function () {
+            await compareBtnEL();
+        });
+    };
+
+    function compareBtnEL() {
+        clearContent();
+        setTimeout(async function () {
+            createCardContainer();
+            updatePlayers();
+            await populateCardContainer([
+                AppController.player1,
+                AppController.player2,
+            ]);
+        }, 500);
+    }
+
+    function updatePlayers() {
+        const player1 = document.querySelector('#player-name-1');
+        const player2 = document.querySelector('#player-name-2');
+        const season1 = document.querySelector('#season-1');
+        const season2 = document.querySelector('#season-2');
+
+        AppController.player1 = AppController.createPlayerObj(
+            player1.value,
+            season1.value
+        );
+        AppController.player2 = AppController.createPlayerObj(
+            player2.value,
+            season2.value
+        );
+    }
+
+    function createCardContainer() {
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
+        contentContainer.appendChild(cardContainer);
+    }
+
+    function populateCardContainer(playerList) {
+        const cardContainer = document.querySelector('.card-container');
+        playerList.forEach(async function (player) {
+            const card = await createPlayerCard(player);
+            cardContainer.appendChild(card);
+        });
+        setTimeout(() => {
+            fadeIn(cardContainer);
+        }, 1000);
+    }
+
+    async function createPlayerCard(player) {
+        const outerDiv = document.createElement('div');
+        const innerDiv = document.createElement('div');
+        const name = document.createElement('h3');
+        const img = new Image();
+
+        img.src = await player.getPlayerPic();
+        name.textContent = await player.fullName();
+        const stats = await player.getSeasonAvg();
+        innerDiv.append(
+            img,
+            name,
+            createStatItem('PPG', stats.pts),
+            createStatItem('REB', stats.reb),
+            createStatItem('APG', stats.ast),
+            createStatItem('FG%', stats.fg_pct),
+            createStatItem('3PT%', stats.fg3_pct),
+            createStatItem('FT%', stats.ft_pct)
+        );
+        innerDiv.classList.add('stat-card');
+        outerDiv.appendChild(innerDiv);
+        outerDiv.classList.add('stat-card-border');
+
+        return outerDiv;
+    }
+
+    function createStatItem(statName, statVal) {
+        const div = document.createElement('div');
+        const key = document.createElement('p');
+        const val = document.createElement('p');
+
+        div.append(key, val);
+        div.classList.add('stat-item');
+        key.textContent = statName;
+        key.style.fontWeight = 'bold';
+        val.textContent = statVal;
+
+        return div;
+    }
+
+    function clearContent() {
+        const content = document.querySelector('.content');
+        fadeOut(content);
+    }
+
+    return { loadDom };
 })();
 
 AppController.runApp();
